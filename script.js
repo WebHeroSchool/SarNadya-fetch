85c7cbace84da16a7002e3994e29e7515c661f57
Надежда Сарнацкая <lutik.ne@gmail.com>
2021-03-14 16:56:42 +0300

    исправила ошибки

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-//добавляем к текущему url "?username=SarNadya"
-//получаем ник пользователя из url
 function getUserName() {
   let user = window.location.search;
   let name = user.split('=')[1];
@@ -13,54 +10,43 @@ function getUserName() {
 }
 
 let username = getUserName();
-
-//добавляем ник пользователя в url
 const url = `https://api.github.com/users/${username}`;
-//получаем данные о пользователе из запроса fetch
-fetch(url)
+const loading = document.getElementById('loading');
+
+const getName = new Promise((resolve, reject) => {
+  setTimeout(() => username ? resolve(username): reject(alert("Информация о пользователе не доступна. Проверьте имя пользователя")), 5000);
+});
+
+const getDate = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let date = new Date();
+    date = date.toDateString();
+    resolve(date);
+  }, 2000);
+});
+
+Promise.all([getName, getDate])
+.then(([username, date]) => document.querySelector('.date').innerHTML = date)
+.then(username => fetch(url))
 .then(response => {
   if (response.status > 200) {
-    username = undefined;
+    throw new Error();
   } else {
   return response.json();
   }
 })
-// .then(response => response.json)
 .then(json => {
   let name = json.name;
   let bio = json.bio;
   let img = new Image();
   img.src = json.avatar_url;
   let userUrl = json.html_url;
-
   document.querySelector('.name').innerHTML = name;
   document.querySelector('.description').innerHTML = bio;
   document.body.append(img);
 })
-// .catch(err => {
-//   document.querySelector('.name').innerHTML = "Информация о пользователе не доступна";
-//   document.querySelector('.description').innerHTML = "Описание пользователя отсутствует";
-// });
-const date = new Date();
-const getDate = date.toDateString();
-
-const loading = document.getElementById('loading');
-console.log(loading);
-
-const getName = new Promise((resolve, reject) => {
-  setTimeout(() => username ? resolve(username): reject(alert("Информация о пользователе не доступна")), 2000);
+.then(load =>  loading.classList.add('off'))
+.catch(err => {
+  alert("Пользователь не найден");
+  console.log(Error.message);
 });
-
-
-const writeDate = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(document.querySelector('.date').innerHTML = getDate), 2000);
-});
-
-// const userLoading = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve(document.querySelector('.off').removeClass, 2000);
-// });
-
- // пока не получим данные о пользователе, загружаем прелоадер
-// const setPreloadr = new Promise((resolve, reject) => {
-//   setTimeout(() => username ? resolve(loading.classList.add('off')), 2000);
-// })
